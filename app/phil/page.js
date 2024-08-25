293d5dc6c1b21e09486ed97217e1f60b8a5476e7
chandrahaas02 <chandrahaas@storyxpress.co>
2024-08-25 10:45:31 +0530

    intial phil

--- app/phil/page.js
+++ app/phil/page.js
@@ -1,8 +1,28 @@
+// "use client"
+// import { useState, useEffect } from "react"
 
-export default function Home() {
-    return (
-      <div className="flex items-center text-2xl">
-        Forbidden Page My Lad You will be drenched into random rants of chandrahaas
+const getGenQuote = async () => {
+  const response = await fetch("https://zenquotes.io/api/random", { cache: 'no-store' });
+  const data = await response.json();
+  // setQuote(quote=>data[0])
+  return data[0]
+}
+
+const quote = await getGenQuote();
+
+
+export default async function Home() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col flex-1 items-center justify-center text-2xl">
+        <div className="text-right">
+          <p>&ldquo;{quote.q}&rdquo;</p>
+          <p>&ndash; {quote.a}</p>
+        </div>
       </div>
-    );
+      <div className="flex-none flex mb-5 items-center justify-center">
+        <p> quotes provided by  <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a></p>
+      </div>
+    </div>
+  );
 }
--- components/Icon.js
+++ components/Icon.js
@@ -126,11 +126,11 @@ export const TickMark = () => {
 
 export const Phil = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg"
-        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-        strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-ban">
-        <circle cx="12" cy="12" r="10"/>
-        <path d="m4.9 4.9 14.2 14.2"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+        viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+        className="lucide lucide-cannabis">
+            <path d="M12 27v-10"/>
+            <path d="M7 12c-1.5 0-4.5 1.5-5 3 3.5 1.5 6 1 6 1-1.5 1.5-2 3.5-2 5 2.5 0 4.5-1.5 6-3 1.5 1.5 3.5 3 6 3 0-1.5-.5-3.5-2-5 0 0 2.5.5 6-1-.5-1.5-3.5-3-5-3 1.5-1 4-4 4-6-2.5 0-5.5 1.5-7 3 0-2.5-.5-5-2-7-1.5 2-2 4.5-2 7-1.5-1.5-4.5-3-7-3 0 2 2.5 5 4 6"/>
         </svg>
     )
 }
